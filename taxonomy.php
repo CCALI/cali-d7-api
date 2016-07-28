6cf7c4b8f6dcea81686e0189081bac3b955d782b
Elmer Masters <emasters@cali.org>
2016-07-28 10:50:40 -0500

    working, now displays taxonomy entities as arrays

--- taxonomy.php
+++ taxonomy.php
@@ -3,14 +3,14 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-CURLOPT_URL => "http://www.cali.org/lessonapi/taxonomy_vocabulary/getTree",
+CURLOPT_URL => "http://www.cali.org/lessonapi/taxonomy_vocabulary/getTree.json",
 CURLOPT_RETURNTRANSFER => true,
 CURLOPT_ENCODING => "",
 CURLOPT_MAXREDIRS => 10,
 CURLOPT_TIMEOUT => 30,
 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 CURLOPT_CUSTOMREQUEST => "POST",
-CURLOPT_POSTFIELDS => "{\"vid\":\"2\"}",
+CURLOPT_POSTFIELDS => "{\"vid\":\"3\",\"load_entities\":\"TRUE\"}",
 CURLOPT_HTTPHEADER => array(
   "cache-control: no-cache",
   "content-type: application/json",
@@ -26,5 +26,7 @@ curl_close($curl);
 if ($err) {
   echo "cURL Error #:" . $err;
   } else {
-  echo $response;
+  echo "<pre>";
+  var_dump(json_decode($response, true));
+  echo "</pre>";
 }
